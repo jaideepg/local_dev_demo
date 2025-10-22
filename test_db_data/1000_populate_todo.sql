20f78e2f51b872a3157dae6670de9d6cd1d10e1c
jaideep <jaideepg@gmail.com>
2025-10-22 10:20:48 -0500

    updated file names

---
+++ test_db_data/1000_populate_todo.sql
@@ -0,0 +1,6 @@
+INSERT INTO todos (title, completed) VALUES ('Buy groceries', FALSE);
+INSERT INTO todos (title, completed) VALUES ('Feed Kitten Mittens', FALSE);
+INSERT INTO todos (title, completed) VALUES ('Dishes - Only if time allows', FALSE);
+INSERT INTO todos (title, completed) VALUES ('Work On Presentation', TRUE);
+INSERT INTO todos (title, completed) VALUES ('Y - Spin Class @ 12pm', TRUE);
+INSERT INTO todos (title, completed) VALUES ('Read a book', FALSE);
--- test_db_data/populate_todo.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO todos (title, completed) VALUES ('Buy groceries & delicious snacks', FALSE);
-INSERT INTO todos (title, completed) VALUES ('Finish presentation', TRUE);
-INSERT INTO todos (title, completed) VALUES ('Feed kitten mittens', FALSE);
-INSERT INTO todos (title, completed) VALUES ('Workout', TRUE);
-INSERT INTO todos (title, completed) VALUES ('Bing netflix', FALSE);
-INSERT INTO todos (title, completed) VALUES ('Dishes - But only if you have time...', FALSE);
