58a7f1bd6a0752578fd755c04eee03b8eaf457a1
jaideep <jaideepg@gmail.com>
2025-10-22 10:16:53 -0500

    updated table data

--- test_db_data/1000_populate_todo.sql
+++ test_db_data/1000_populate_todo.sql
@@ -1,5 +1,6 @@
 INSERT INTO todos (title, completed) VALUES ('Buy groceries', FALSE);
-INSERT INTO todos (title, completed) VALUES ('Finish project report', TRUE);
-INSERT INTO todos (title, completed) VALUES ('Call mom', FALSE);
-INSERT INTO todos (title, completed) VALUES ('Workout session', TRUE);
+INSERT INTO todos (title, completed) VALUES ('Feed Kitten Mittens', FALSE);
+INSERT INTO todos (title, completed) VALUES ('Dishes - Only if time allows', FALSE);
+INSERT INTO todos (title, completed) VALUES ('Work On Presentation', TRUE);
+INSERT INTO todos (title, completed) VALUES ('Y - Spin Class @ 12pm', TRUE);
 INSERT INTO todos (title, completed) VALUES ('Read a book', FALSE);
